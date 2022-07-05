dd4b8e4f8cd04291a76ac74b7b9df302047f77ce
adityalochan <aditya.lochan25@gmail.com>
2022-07-04 23:48:11 -0400

    Structuring Project

--- HotelReservationApp/src/service/ReservationService.java
+++ HotelReservationApp/src/service/ReservationService.java
@@ -1,10 +1,32 @@
 package service;
 
+import model.Customer;
 import model.IRoom;
+import model.Reservation;
+import model.Room;
+
+import java.util.Collection;
+import java.util.Date;
 
 public class ReservationService {
 
     public void addRoom(IRoom room){
 //        IRoom
+    }
+    
+    public Room getARoom(String roomId){
+
+    }
+
+    public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+
+    }
+
+    public Collection<Reservation> getCustomerReservation(Customer customer){
+
+    }
+
+    public void printAllReservation(){
+
     }
 }
