d76c009011b72013c813dad283f78e1b0d46c4bf
adityalochan <aditya.lochan25@gmail.com>
2022-07-05 17:26:35 -0400

    Added UI Structure

---
+++ HotelReservationApp/src/ui/AdminMenu.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class AdminMenu {
+
+    public AdminMenu(){
+        System.out.println("1. See all Customers");
+        System.out.println("2. See all rooms");
+        System.out.println("3. See all reservations");
+        System.out.println("4. Add a room");
+        System.out.println("5. Back to main menu");
+    }
+}
